2ca94b82abd2fa5a3773aef923cd39e3b84bc530
Shinora <simonamato23@gmail.com>
2022-06-14 00:14:35 +0200

    real time data selection

--- spotify_analyze_song.py
+++ spotify_analyze_song.py
@@ -1,6 +1,7 @@
+from numpy import float64
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import pyqtgraph.examples
+import pyqtgraph
 
 class Spotipy_Instance(): # Create env var SPOTIPY_CLIENT_ID SPOTIPY_CLIENT_SECRET and SPOTIPY_REDIRECT_URI ( export SPOTIPY...=secret_value)
 
@@ -10,9 +11,15 @@ class Spotipy_Instance(): # Create env var SPOTIPY_CLIENT_ID SPOTIPY_CLIENT_SECR
     raw_analysis = None
     raw_features = None
     player_time = None
+    song_name = None
+    song_image = None
+    song_artist = None
+    song_popularity = None
+
 
     def __init__(self):
         self.spotipy = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=self.scope))
+
 
     def update_current_song_id(self):
         results = self.spotipy.currently_playing(market='FR')
@@ -39,7 +46,8 @@ class Spotipy_Instance(): # Create env var SPOTIPY_CLIENT_ID SPOTIPY_CLIENT_SECR
     
     def get_current_song_time(self):
         self.update_current_song_id()
-        self.player_time = self.spotipy.audio_features(self.played_id).get('progress_ms')
+        self.player_time = self.spotipy.current_playback(market='FR').get('progress_ms')
+        #self.player_time = [self.spotipy.audio_features(self.played_id)[i].get('progress_ms') for i in range(0,len(self.spotipy.audio_features(self.played_id)))]
         return self.player_time
 
     def get_info_track(self):
@@ -54,11 +62,37 @@ class Spotipy_Instance(): # Create env var SPOTIPY_CLIENT_ID SPOTIPY_CLIENT_SECR
     def get_metadata(self):
         pass
 
-    def update(self):
+    def update_data(self):
+        self.get_info_track()
         self.get_current_song_analysis()
         self.get_current_song_time()
 
+    def get_current_data(self):
+        time = self.get_current_song_time()
+        data = self.get_current_song_analysis()
+        current_data = None
+        #print(data)
+        with open('data.txt', "w") as datafile:
+            datafile.write(str(data))
+        
+        bars = data.get('bars')
+        beats = data.get('beats')
+        sections = data.get('sections')
+        segments = data.get('segments')
+        
+        for element in segments:
+            
+            if time - element.get('start')*1000 <= -500 and time - element.get('start')*1000 < 0:
+                current_data = element
+
+        return current_data
+
+    def draw(self):
+        data = self.get_current_data()
+
+
 
 sp = Spotipy_Instance()
-sp.get_info_track()
-pyqtgraph.examples.run()
+
+
+print(sp.get_current_data())
